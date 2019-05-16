a7d1e374380630a5443baf43a96eaf572ec005be
Karl <jinweixing1006@gmail.com>
2019-05-16 12:20:18 -0700

    init

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ana',
+      version='0.1',
+      description='ana test',
+      url='https://github.com/KarlXing/Ana',
+      author='Jinwei Xing',
+      author_email='jinweixing1006@gmail.com',
+      license='MIT',
+      packages=['ana'],
+      zip_safe=False)
